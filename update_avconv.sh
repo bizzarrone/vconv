9752587a9b93dcbbba093699aad4b5a1ef73d058
Bizzarrone <bizzarrone@gmail.com>
2016-06-08 13:38:56 +0000

    Commit

--- update_avconv.sh
+++ update_avconv.sh
@@ -3,10 +3,10 @@
 
 rm -rf /scripts
 mv -f /CONDIVISA /CONDIVISA-OLD
-mv -f /vconv /vconv-old
+rm -rf /vconv 
 mv -f /etc/init.d/rc.local /etc/init.d/rc.local.old
-mv -f /var/www/html /var/www/html-old
+rm -rf /var/www/html 
 cd
-rm -f install_avconv.sh
+rm -f install_avconv.sh*
 wget https://raw.githubusercontent.com/bizzarrone/vconv/master/install_avconv.sh
 sh install_avconv.sh 
